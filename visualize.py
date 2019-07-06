64369d9872f3a82a9960d01b66fef1b23f55996f
mattrossman <mrossman@umass.edu>
2019-07-05 20:41:00 -0500

    Move axis transform calculations to initialization

--- visualize.py
+++ visualize.py
@@ -6,9 +6,11 @@ from matplotlib import animation
 
 
 df_skeleton = pd.read_csv('jumping-jacks.csv', engine='python', index_col=0)
-df_pos_x = df_skeleton.filter(regex="PositionX")
-df_pos_y = df_skeleton.filter(regex="PositionY")
-df_pos_z = df_skeleton.filter(regex="PositionZ")
+
+# Compile dimensional dataframes, transform to better fit the real scene
+df_pos_x = df_skeleton.filter(regex="PositionX") * -1
+df_pos_y = df_skeleton.filter(regex="PositionZ") - 2
+df_pos_z = df_skeleton.filter(regex="PositionY")
 
 # Attaching 3D axis to the figure
 fig = plt.figure()
@@ -27,19 +29,24 @@ ax.set_zlabel('Z')
 
 title = ax.set_title('3D Test')
 
-start_frame = 100
-points, = plt.plot(df_pos_x.iloc[start_frame], df_pos_z.iloc[start_frame]-2, df_pos_y.iloc[start_frame], linestyle="", marker="o")
+start_frame = 0
+points, = plt.plot(df_pos_x.iloc[start_frame], df_pos_y.iloc[start_frame], df_pos_z.iloc[start_frame], linestyle="", marker="o")
 
 
 def update_graph(num):
-    points.set_data(df_pos_x.iloc[num], df_pos_z.iloc[num]-2)
-    points.set_3d_properties(df_pos_y.iloc[num])
+    points.set_data(df_pos_x.iloc[num], df_pos_y.iloc[num])
+    points.set_3d_properties(df_pos_z.iloc[num])
     title.set_text('3D Test, time={}'.format(num))
 
 
 ani = animation.FuncAnimation(fig, update_graph, len(df_skeleton),
                               interval=10, blit=False)
-Writer = animation.writers['ffmpeg']
-writer = Writer(fps=24, metadata=dict(artist='Me'), bitrate=1800)
-ani.save('skeleton.mp4', writer=writer)
+
+
+def write_animaton():
+    Writer = animation.writers['ffmpeg']
+    writer = Writer(fps=24, metadata=dict(artist='Me'), bitrate=1800)
+    ani.save('skeleton.mp4', writer=writer)
+
+
 plt.show()

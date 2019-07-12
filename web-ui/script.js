a4834be8a8b767de1b42b25e69bcfe3415751de7
mattrossman <mrossman@umass.edu>
2019-07-12 16:33:56 -0500

    Add basic JSON import script

---
+++ web-ui/script.js
@@ -0,0 +1,19 @@
+function loadJSON(callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'dummy-segments.json', true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            callback(JSON.parse(xobj.responseText));
+        }
+    };
+    xobj.send(null);
+}
+
+var segments;
+
+var myCallback = function(json) {
+    segments = json;
+}
+
+loadJSON(myCallback);

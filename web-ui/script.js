5ed822f0feeed31c2e8ee64d9d37a8fe4c15c0e4
Tanner Amundsen <tamunds1@jhu.edu>
2019-07-18 14:39:53 -0500

    Added rendering. Still need to fix auto width adjustment

--- web-ui/script.js
+++ web-ui/script.js
@@ -140,6 +140,43 @@ var myCallback = function(json) {
     Plotly.newPlot('plot', plotlyGetInitData(properties), plotlyGetInitLayout(properties), {responsive: true});
 }
 
+//ThreeJS Renderer
+var camera, scene, renderer;
+var geometry, material, mesh;
+
+init();
+animate();
+
+function init() {
+
+    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+    camera.position.z = 1;
+
+    scene = new THREE.Scene();
+
+    geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+    material = new THREE.MeshNormalMaterial();
+
+    mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
+
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.getElementById('rendering').appendChild( renderer.domElement );
+
+}
+
+function animate() {
+
+    requestAnimationFrame( animate );
+
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.02;
+
+    renderer.render( scene, camera );
+
+}
+
 function plotlyGetInitLayout(properties) {
     const numActive = Object.values(properties).filter(p => p.active).length
     const layout = {
@@ -194,6 +231,7 @@ function plotlyGetRelayout(properties) {
         'grid.yaxes': Object.values(properties).filter(p => p.active).map(p => p.trace.yaxis)
     };
     return layout;
+
 }
 
 loadJSON(myCallback);

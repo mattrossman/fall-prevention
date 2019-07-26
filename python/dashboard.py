f24084a56e5f26e510fc546fe1b123eb2a7cb63b
mattrossman <mrossman@umass.edu>
2019-07-26 16:18:19 -0500

    Tweak dashboard, abstract framerate

--- python/dashboard.py
+++ python/dashboard.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 from smartfloor import Floor
 from kinect import KinectRecording
 import sys
+from scipy.signal import argrelextrema
 
 walk_segments = [
     {
@@ -15,17 +16,25 @@ walk_segments = [
         'end': '2019-07-19T22:53:04'
     },
     {
-        'floor_path': 'data/time-sync-walk-2.csv',
+        'pressure_path': 'data/time-sync-walk-2.csv',
         'start': '2019-07-19T22:53:43',
         'end': '2019-07-19T22:53:49'
+    },
+    {
+        'pressure_path': 'data/time-sync-walk-4/smartfloor.csv',
+        'rgb_path': 'data/time-sync-walk-4/Color',
+        'start': '2019-07-19 22:56:10',
+        'end': '2019-07-19 22:56:21'
     }
 ]
 segment = walk_segments[0]
 
 """ SET UP SOURCE DATA """
+framerate_hz = 25
+frame_delay = 1000/framerate_hz
 kr = KinectRecording(segment['rgb_path'])
 floor = Floor.from_csv(segment['pressure_path'])
-samples = pd.date_range(segment['start'], segment['end'], freq='40ms')
+samples = pd.date_range(segment['start'], segment['end'], freq=pd.Timedelta(frame_delay, 'ms'))
 pressure = floor.denoise().interp(time=samples)
 cop = floor.cop().interp(time=samples)
 speed = floor.cop_speed().interp(time=samples)
@@ -59,20 +68,23 @@ img = ax1.imshow(kr.imread(samples[0]))
 quad = pressure.isel(time=0).plot(ax=ax2, vmin=0, vmax=1023, add_colorbar=False)
 speed.rolling(time=2, center=True).mean().plot(ax=ax3)
 (speed - speed.shift(time=1)).rolling(time=2).mean().plot()
+(cop.magnitude / 1024 * 20).plot()
 scrub_line = ax3.axvline(samples[0], c='r')
 cop_dot = ax2.plot(0, 0, 'ro')
 ax1.set_axis_off()
 ax2.set_axis_off()
 ax2.set_title('')
+ax2.invert_yaxis()
 ax2.set_aspect('equal', adjustable='box')
 plt.xticks(size=6, rotation='horizontal', ha='center')  # Smaller timestamp labels
 plt.tight_layout(pad=0.4, w_pad=0.5)
 update_frame(0)
 
-ani = animation.FuncAnimation(fig, update_frame, frames=samples.size,
-                              interval=1000, save_count=sys.maxsize)
 
-def write_animaton(path):
-    Writer = animation.writers['ffmpeg']
-    writer = Writer(fps=25, metadata=dict(artist='Me'), bitrate=1800)
-    ani.save(path, writer=writer)
+def animate(path=None):
+    ani = animation.FuncAnimation(fig, update_frame, frames=samples.size,
+                                  interval=1000, save_count=sys.maxsize)
+    if path is not None:
+        Writer = animation.writers['ffmpeg']
+        writer = Writer(fps=framerate_hz, metadata=dict(artist='Me'), bitrate=1800)
+        ani.save(path, writer=writer)

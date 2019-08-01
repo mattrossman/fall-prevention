eada8aaf77ecf29ef3d2512838c4a8eacfea5444
mattrossman <mrossman@umass.edu>
2019-08-01 12:43:32 -0500

    Organize plotting code

--- python/dashboard.py
+++ python/dashboard.py
@@ -9,9 +9,6 @@ from smartfloor import Floor
 from kinect import KinectRecording
 from matplotlib.gridspec import GridSpec
 import sys
-from scipy.signal import argrelmin, argrelmax
-import xarray.ufuncs as xu
-import xarray as xr
 
 walk_segments = [
     {
@@ -49,33 +46,39 @@ window = int(framerate_hz / 25 * smoothing)
 kr = KinectRecording(segment['rgb_path'])
 floor = Floor.from_csv(segment['pressure_path'], freq=pd.Timedelta(frame_delay, 'ms'), start=segment['start'], end=segment['end'])
 samples = pd.DatetimeIndex(floor.samples.time.values)
+
+""" CACHE SOME VARIABLES """
 pressure = floor.pressure
 cop = floor.cop
 speed = floor.cop_speed.rolling(time=window, center=True).mean()
 delta_speed = floor.cop_delta_speed.rolling(time=window, center=True).mean()
+steps = floor.footsteps
+cop_mlap = floor.cop_mlap
+cop_vel_mlap = floor.cop_vel_mlap
+
 
 def update_fig(i):
     dt = samples[i]
     fig.suptitle(f'Time: {dt.strftime("%H:%M:%S:%f")}')
-    """ TOP PLOT """
+    # TOP LEFT
     if img is not None:
         img.set_data(kr.imread(dt))
 
-    """ MIDDLE PLOT """
+    # TOP RIGHT
     quad.set_array(pressure.isel(time=i).stack(z=('y', 'x')))
 
-    """ BOTTOM PLOT """
+    # BOTTOM
     x, y, mag = cop.isel(time=i).to_array()
     scrub_line.set_data([dt, dt], [0, 1])
     cop_dot[0].set_data(x, y)
     cop_dot[0].set_markersize(10 * mag / cop.magnitude.max(dim='time'))
 
-    """ VERTICAL PLOT """
+    # VERTICAL
     scrub_line_v.set_data([0, 1], [dt, dt])
 
 
 """ SET UP GRID LAYOUT """
-fig = plt.figure(figsize=(13, 7))
+fig = plt.figure(figsize=(15, 7))
 gridspec = GridSpec(2, 3, width_ratios=[3, 3, 1], height_ratios=[2, 1])
 ax1 = fig.add_subplot(gridspec.new_subplotspec((0, 0), rowspan=1, colspan=1))
 ax2 = fig.add_subplot(gridspec.new_subplotspec((0, 1), rowspan=1, colspan=1))
@@ -83,22 +86,53 @@ ax3 = fig.add_subplot(gridspec.new_subplotspec((1, 0), rowspan=1, colspan=2))
 ax4 = fig.add_subplot(gridspec.new_subplotspec((0, 2), rowspan=2, colspan=1))
 
 """ INIT PLOTS """
+
+# TOP LEFT
+ax1.set_axis_off()
 try:
     img = ax1.imshow(kr.imread(samples[0]))
 except FileNotFoundError:
     img = None
-quad = pressure.isel(time=0).plot(ax=ax2, vmin=0, vmax=1023, add_colorbar=False)
-floor.cop_speed.plot(ax=ax3)
 
-scrub_line = ax3.axvline(samples[0], c='r')
-cop_dot = ax2.plot(0, 0, 'ro')
-ax1.set_axis_off()
+# TOP RIGHT
 ax2.set_axis_off()
 ax2.set_title('')
-ax2.invert_yaxis()
 ax2.set_aspect('equal', adjustable='box')
-plt.tight_layout(pad=0.4, w_pad=0.5)
-ax3.set_xticklabels(ax3.get_xticklabels(), ha='center', rotation='horizontal', size=6)
+quad = pressure.isel(time=0).plot(ax=ax2, vmin=0, vmax=1023, add_colorbar=False)
+cop_dot = ax2.plot(0, 0, 'ro')
+ax2.invert_yaxis()
+
+rights = cop.sel(time=steps.dir[steps.dir == 'right'].time)
+lefts = cop.sel(time=steps.dir[steps.dir == 'left'].time)
+ax2.scatter(rights.x, rights.y, c='yellow')
+ax2.scatter(lefts.x, lefts.y, c='green')
+
+start_mid, end_mid = floor.walk_line
+ax2.plot([start_mid.x, end_mid.x], [start_mid.y, end_mid.y], c='r')
+
+# BOTTOM
+floor.cop_speed.plot(ax=ax3)
+scrub_line = ax3.axvline(samples[0], c='r')
+for step_time in steps.time.values:
+    ax3.axvline(step_time, c='k', linestyle='--')
+plt.setp(ax3.xaxis.get_majorticklabels(), rotation='horizontal', ha='center', size=6)
+
+# VERTICAL
+ax4.plot(cop_mlap.med, cop_mlap.time.values)
+ax4.plot(cop_vel_mlap.med / 5, cop_vel_mlap.time.values)
+ax4.axvline(0, c='k')
+ax4.axis(ymin=samples[0], ymax=samples[-1])
+plt.setp(ax4.yaxis.get_majorticklabels(), rotation='vertical', va='center', size=6)
+# ax4.set_axis_off()
+scrub_line_v = ax4.axhline(samples[0], c='r')
+
+for step_time in steps.time.values:
+    ax4.axhline(step_time, c='k', linestyle='--')
+
+
+plt.tight_layout(pad=0, w_pad=0.4)
+
+""" HANDLERS """
 
 
 def onclick(event):
@@ -108,9 +142,6 @@ def onclick(event):
             dt = mdates.num2date(event.xdata)
             i = samples.get_loc(dt, method='nearest')
             update_fig(i)
-
-
-cid = fig.canvas.mpl_connect('button_press_event', onclick)
 
 
 def update_frame(i):
@@ -128,34 +159,5 @@ def animate(path=None):
         ani.save(path, writer=writer)
 
 
-""" PLOT THE FOOTSTEP MARKERS """
-steps = floor.footsteps
-
-for step_time in steps.time.values:
-    ax3.axvline(step_time, c='k', linestyle='--')
-
-rights = cop.sel(time=steps.dir[steps.dir == 'right'].time)
-lefts = cop.sel(time=steps.dir[steps.dir == 'left'].time)
-
-ax2.scatter(rights.x, rights.y, c='yellow')
-ax2.scatter(lefts.x, lefts.y, c='green')
-
-""" PLOT THE WALK LINE """
-start_mid, end_mid = floor.walk_line
-ax2.plot([start_mid.x, end_mid.x], [start_mid.y, end_mid.y], c='r')
-
-cop_mlap = floor.cop_mlap
-cop_vel_mlap = floor.cop_vel_mlap
-ax4.plot(cop_mlap.med, cop_mlap.time.values)
-ax4.plot(cop_vel_mlap.med / 5, cop_vel_mlap.time.values)
-ax4.axvline(0, c='k')
-ax4.axis(ymin=samples[0], ymax=samples[-1])
-ax4.set_xticklabels(ax4.get_xticklabels(), ha='center', rotation='horizontal', size=6)
-ax4.set_yticklabels(ax4.get_yticklabels(), va='center', rotation='vertical', size=6)
-ax4.set_axis_off()
-scrub_line_v = ax4.axhline(samples[0], c='r')
-
-for step_time in steps.time.values:
-    ax4.axhline(step_time, c='k', linestyle='--')
-
+cid = fig.canvas.mpl_connect('button_press_event', onclick)
 update_fig(0)

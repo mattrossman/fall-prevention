cbe23f1e7d78742a44ac9f8fc291d9637e67dbdd
mattrossman <mrossman@umass.edu>
2019-08-06 21:14:53 -0500

    Add test run walk segments

--- python/segments.py
+++ python/segments.py
@@ -28,3 +28,47 @@ time_sync = [
         'end': '2019-07-19 22:56:21'
     }
 ]
+test_run = [
+    {
+        'name': 'matt_hunch_1',
+        'pressure_path': 'data/8-6/matt_hunch_1.csv',
+        'rgb_path': '',
+        'start': '2019-08-06 21:10:34.383',
+        'end': '2019-08-06 21:10:43.343'
+    },
+    {
+        'name': 'matt_normal_1',
+        'pressure_path': 'data/8-6/matt_normal_1.csv',
+        'rgb_path': '',
+        'start': '2019-08-06 21:03:59.426',
+        'end': '2019-08-06 21:04:02.706'
+    },
+    {
+        'name': 'matt_slow_1',
+        'pressure_path': 'data/8-6/matt_slow_1.csv',
+        'rgb_path': '',
+        'start': '2019-08-06 21:08:27.956',
+        'end': '2019-08-06 21:08:35.156'
+    },
+    {
+        'name': 'tnnr_hunch_1',
+        'pressure_path': 'data/8-6/tnnr_hunch_1.csv',
+        'rgb_path': '',
+        'start': '2019-08-06 21:11:29.009',
+        'end': '2019-08-06 21:11:37.809'
+    },
+    {
+        'name': 'tnnr_normal_1',
+        'pressure_path': 'data/8-6/tnnr_normal_1.csv',
+        'rgb_path': '',
+        'start': '2019-08-06 21:07:27.92',
+        'end': '2019-08-06 21:07:32.32'
+    },
+    {
+        'name': 'tnnr_slow_1',
+        'pressure_path': 'data/8-6/tnnr_slow_1.csv',
+        'rgb_path': '',
+        'start': '2019-08-06 21:09:18.879',
+        'end': '2019-08-06 21:09:24.879'
+    },
+]

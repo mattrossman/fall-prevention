2b712d7c785be7c4512cf9ea37ab4f887b2278b3
Tanner Amundsen <tamunds1@jhu.edu>
2019-08-09 21:16:06 -0500

    Added loop to calculate accuracy

--- python/results.py
+++ python/results.py
@@ -1,28 +1,54 @@
 import os
 import re
+import pickle
 from scipy import spatial
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 from smartfloor import FloorRecording, FloorRecordingBatch
 directory = 'data/08-07-2019'
+num_subjects = 7
 
-# Get training and testing source data paths
-regex_test = re.compile(r'.*7_.*')
-train_paths = [f'{directory}/{filename}' for filename in os.listdir(directory) if not regex_test.match(filename)]
-test_paths = [f'{directory}/{filename}' for filename in os.listdir(directory) if regex_test.match(filename)]
 
-# Extract gait cycles
-train_recordings = [FloorRecording.from_csv(path, trimmed=True) for path in train_paths]
-train_batch = FloorRecordingBatch(train_recordings)
-train_cycles = train_batch.gait_cycles
+def calculate_accuracy(i):
+    # Get training and testing source data paths
+    regex_test = re.compile(r'.*' + str(i) + '_.*')
+    train_paths = [f'{directory}/{filename}' for filename in os.listdir(directory) if not regex_test.match(filename)]
+    # test_paths = [f'{directory}/{filename}' for filename in os.listdir(directory) if regex_test.match(filename)]
 
-recording_test = FloorRecording.from_csv(f'{directory}/7_slow_1.csv', trimmed=True)
-test_cycles = recording_test.gait_cycles
+    # Extract gait cycles
+    train_recordings = [FloorRecording.from_csv(path, trimmed=True) for path in train_paths]
+    train_batch = FloorRecordingBatch(train_recordings)
+    train_cycles = train_batch.gait_cycles
+    gait_styles = {
+        'normal': ['normal', {}],
+        'slow': ['slow', {}],
+        'hunch': ['hunch', {}],
+        'steppage': ['stppg', {}],
+        'left_hobble': ['lhob', {}],
+        'right_hobble': ['rhob', {}]
+    }
 
-train_feats = [cycle.features for cycle in train_cycles]
-tree = spatial.KDTree(train_feats)
+    with open('tree' + str(i) + '.p', 'rb') as file:
+        tree = pickle.load(file)
 
-top5 = [train_cycles[i] for i in tree.query(test_cycles[0].features, k=5)[1]]
+        for style in gait_styles.values():
+            file_string = str(i) + '_' + style[0] + '_1.csv'
+            recording_test = FloorRecording.from_csv(f'{directory}/' + file_string, trimmed=True)
+            test_cycles = recording_test.gait_cycles
+
+            count_style_total = 0
+            count_style_correct = 0
+            for j in range(len(test_cycles)):
+                count_style_total = count_style_total + 1
+                top5 = [train_cycles[l] for l in tree.query(test_cycles[j].features, k=5)[1]]
+                top1 = top5[0].name[2:]
+                top1_type = top1[:top1.find('_')]
+                if style[0] == top1_type:
+                    count_style_correct = count_style_correct + 1
+            style[1].update({'total': count_style_total})
+            style[1].update({'total_correct': count_style_correct})
+            style[1].update({'accuracy': str((count_style_correct / count_style_total) * 100) + '%'})
+    return gait_styles
 
 
 def plot_motion_similarity():
@@ -50,4 +76,6 @@ def plot_motion_similarity():
                angles='xy', units='dots', width=5, pivot='mid', cmap='cool')
     # ax2.set_xlim(-0.5, 0.5)
 
-plot_motion_similarity()
+
+# plot_motion_similarity()
+participants = [calculate_accuracy(i) for i in range(1, num_subjects + 1)]
